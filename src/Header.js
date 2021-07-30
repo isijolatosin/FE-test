8b42bfee13561b81cf7f1659aa30300a196f29ce
isijolatosin <isijolatosin2210@gmail.com>
2021-07-30 06:15:21 -0400

    header logo adjust

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,11 @@ import { sunwingLogo } from './fetchData';
 function Header() {
   return (
     <div
-      style={{ backgroundColor: '#ff6000', height: 60 }}
-      className='pl-5 lg:pl-5 2xl:pl-48 xl:pl-5'
+      style={{ backgroundColor: '#ff6000', height: 80 }}
+      className='pl-5 lg:pl-5 2xl:pl-48 xl:pl-5 rounded-br-3xl'
     >
       <Link to='/'>
-        <img className='pt-5' width='100' src={sunwingLogo} alt='Sunwing' />
+        <img className='pt-7' width='100' src={sunwingLogo} alt='Sunwing' />
       </Link>
       <div></div>
     </div>
